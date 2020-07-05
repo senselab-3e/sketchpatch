a7fb9a657017744d3c493fccf2f6e5536f20b1bb
essitam <matisseam@hotmail.com>
2020-07-05 14:28:04 -0400

    eee

--- entryway/wp-content/themes/writee/inc/functions/widgets.php
+++
@@ -1,9 +0,0 @@
-<?php 
-/*******************************************/
-## Custom widget list
-/*******************************************/
-
-require_once('widgets/recent-posts.php');
-require_once('widgets/social-icons.php');
-require_once('widgets/about-me.php');
-?>
